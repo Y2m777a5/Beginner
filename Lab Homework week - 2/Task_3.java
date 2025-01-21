045c1f3cade059c48ca5a4aa524b34b7102e4cd7
Y2m777a5 <orgdotin222@gmail.com>
2025-01-21 17:59:03 +0600

    a
    b

---
+++ Lab Homework week - 2/Task_3.java
@@ -0,0 +1,13 @@
+
+public class Task_3 {
+  
+  
+  public static void main(String[] args) { 
+    int rad = 4;
+    double pi = 3.1416d;
+    double circumference = (2*pi*rad);
+    double area = (pi*rad*rad);
+    System.out.println("Circumference: "+circumference);
+    System.out.println("Area: "+area);
+  } 
+}
---
+++ Lab Homework week - 2/Task_3_2.java
@@ -0,0 +1,19 @@
+
+public class Task_3_2 {
+  
+  
+  public static void main(String[] args) 
+  { 
+    int num1 = 24;
+    int num2 = 20;
+    System.out.println("Previous numbers: num1 = "+num1+", num2 = "+num2);
+    
+    System.out.println("After swaping num1 = "+num2);
+    num2 = num1;
+    System.out.println("After swaping num2 = "+num1);
+    
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
