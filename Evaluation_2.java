b79da53e2379d6d7226cc4aab2ca6a43db1dfa1b
Y2m777a5 <orgdotin222@gmail.com>
2025-01-21 17:56:57 +0600

    Lab cse110
    Lab classwork week 2

---
+++ Evaluation_2.java
@@ -0,0 +1,19 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Evaluation_2 {
+  
+  
+  public static void main(String[] args) 
+  { 
+    int sid = 23221454;
+    int digit = sid % 100;
+    int digit1 = sid % 10;
+    int digit2 = digit / 10;
+    
+    System.out.println(digit1);
+    System.out.println(digit2);
+  
+  }
+  
+}
---
+++ Task_2_5.java
@@ -0,0 +1,19 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Task_2_5 {
+  
+  
+  public static void main(String[] args) 
+  { 
+    int num11 = 24;
+    double num12 = 10;
+    
+    System.out.println("Summation : "+(num11+num12));
+    System.out.println("Product : "+(num11*num12));
+    System.out.println("Division : "+(num11/num12));
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
