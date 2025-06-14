32ab36608398d00f84c89d815c4f131521418b30
Y2m777a5 <orgdotin222@gmail.com>
2025-06-15 01:45:24 +0600

    DEL Java

--- JAVA/Alpha_pattern.java
+++
@@ -1,9 +0,0 @@
-public class Alpha_pattern {
-    public static void main(String[] args){
-        String[] A = {"   *" ," *   *", "*     *", "*******", "*     *", "*     *", "*     *"};
-        for (int i = 0; i< (A.length-1); i++){
-            System.out.println(A[i]);
-        }
-
-    }
-}
