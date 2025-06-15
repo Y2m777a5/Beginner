e90e03a965c58dc1d9f89dd21214606e9fc96df1
Y2m777a5 <orgdotin222@gmail.com>
2025-06-15 17:00:46 +0600

    Conditional Cpp
    Greatest and Smallest number

--- C++/Conditional/Small_Large_Number.cpp
+++ C++/Conditional/Small_Large_Number.cpp
@@ -3,36 +3,36 @@ using namespace std;
 
 int main()
 {
-    int a,b,c;
-    cout << "\033[1;96m" << "\n\nEnter 1st number:" << "\033[97m" << endl;
-    scanf("%d", &a);
+    long a,b,c;
+    cout << "\033[1;96m" << "\nEnter 1st number:" << "\033[97m" << endl;
+    scanf("%ld", &a);
     cout << "\033[1;96m" << "\nEnter 2nd number:" << "\033[97m" << endl;
-    scanf("%d", &b);
+    scanf("%ld", &b);
     cout << "\033[1;96m" << "\nEnter 3rd number:" << "\033[97m" << endl;
-    scanf("%d", &c);
+    scanf("%ld", &c);
     cout << "\n>> \033[38;2;170;255;0m";
 
-    //Largest Number check;
+    //Greatest Number check;
     if(a>b && a>c){
-        printf("Greatest number: \033[1;95m%d",a);
+        printf("Greatest number: \033[1;95m%ld",a);
     }
     else if(b>a && b>c){
-        printf("Greatest number: \033[1;95m%d",b);
+        printf("Greatest number: \033[1;95m%ld",b);
     }
     else{
-        printf("Greatest number: \033[1;95m%d",c);
+        printf("Greatest number: \033[1;95m%ld",c);
     }
     cout << "\033[97m\n>> ";
     cout << "\033[38;2;170;255;0m";
    //Smallest Numbers check;
     if(a<b && a<c){
-        printf("Smallest number: \033[1;95m%d",a);
+        printf("Smallest number: \033[1;95m%ld",a);
     }
     else if(b<a && b<c){
-        printf("Smallest number: \033[1;95m%d",b);
+        printf("Smallest number: \033[1;95m%ld",b);
     }
     else{
-        printf("Smallest number: \033[1;95m%d",c);
+        printf("Smallest number: \033[1;95m%ld",c);
     }
     cout << "\033[0m \n" << endl;
     return 0;
